97615beb6f6c71a36f86dfd6add49e1e50eaa1be
tanduong <tanduong@live.com>
2020-02-13 07:22:43 -0600

    PLAT 1223

--- FeedBlueprint/navigator.js
+++ FeedBlueprint/navigator.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from "react-navigation-stack";
 import Feed from "./screens/feed";
 import Home from "./screens";
 
-export const FeedBlueprintNavigator = createStackNavigator(
+export default FeedBlueprintNavigator = createStackNavigator(
   {
     Home: { screen: Home },
     Feed: { screen: Feed },
--- install.sh
+++ install.sh
@@ -16,14 +16,24 @@ source blueprint-lib/init.sh
 # APP_NAME is the name of the Django app that will be modified
 ##
 
-BLUEPRINT="FeedBlueprint"
-NAME="Feed"
+BLUEPRINT="$3"
+NAME="$4"
+
+if [ -z "$BLUEPRINT" ]
+then
+    BLUEPRINT="FeedBlueprint"
+fi
+
+if [ -z "$NAME" ] 
+then
+    NAME="Feed"
+fi
 
 EXT_POINT_1="@BlueprintInsertion"
 EXT_POINT_2="@BlueprintImportInsertion"
 EXT_POINT_3="@BlueprintNavigationInsertion"
 DATA_1="{ name: '${BLUEPRINT}', human_name: '${NAME}', access_route: '${BLUEPRINT}'},"
-DATA_2="import { ${BLUEPRINT}Navigator } from '..\/features\/${BLUEPRINT}\/navigator';"
+DATA_2="import ${BLUEPRINT}Navigator from '..\/features\/${BLUEPRINT}\/navigator';"
 DATA_3="${BLUEPRINT}: { screen: ${BLUEPRINT}Navigator },"
 
 echo "create blueprint folder"
